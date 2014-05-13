220f51c780c6dd87576dba70a497d253cb4c3e6c
TheRagNarok <bensmith222@hotmail.co.uk>
2014-05-13 12:42:47 +0100

    sdg

--- www/js/index.js
+++ www/js/index.js
@@ -29,9 +29,9 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-				$('#page').live('pageshow', function(event){
-    onDeviceReady();             
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+				//$('#page').live('pageshow', function(event){
+  //  onDeviceReady();             
 });
     },
     // deviceready Event Handler
