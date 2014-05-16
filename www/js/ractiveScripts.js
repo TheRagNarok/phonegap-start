421a037f2de881b28b610feb2bf1bb104c9dba59
TheRagNarok <bensmith222@hotmail.co.uk>
2014-05-16 11:42:56 +0100

    cc

--- www/js/ractiveScripts.js
+++ www/js/ractiveScripts.js
@@ -131,11 +131,11 @@ addF = new Ractive ({ // add film page ractve
 		
 		<!----------------------------------- reset app ----------------------------------------------------->	
 	
-		//reset: function ( event ) {  //linked to reset button, used to reset app local storage to defaults
-		//	window.localStorage.removeItem('first');
-	    //  alert("app reset");
-		//	window.location = "index.html";
-		//},
+		reset: function ( event ) {  //linked to reset button, used to reset app local storage to defaults
+			window.localStorage.removeItem('first');
+	        alert("app reset");
+			window.location = "index.html";
+		},
 		
 		
 		
